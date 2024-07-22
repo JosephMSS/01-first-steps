9cb1ff544702e4772d834adba14603bb3ba59748
Joseph Morales Silva <josephzx1@hotmail.com>
2024-07-21 18:48:17 -0600

    refactor: crear routes interface

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,12 +1,46 @@
-export const Navbar = () => {
+import { HomeIcon } from "@primer/octicons-react"
+import Link from "next/link"
+export enum PATHS {
+    ABOUT = '/about',
+    PRICING = '/pricing',
+    CONTACT = '/contact'
+
+}
+interface Routes {
+    path: PATHS,
+    text: string
+}
+const nevItems: Routes[] = [
+    {
+        path: PATHS.ABOUT,
+        text: 'about'
+    },
+    {
+        path: PATHS.PRICING,
+        text: 'pricing'
+    },
+    {
+        path: PATHS.CONTACT,
+        text: 'contact'
+    }
+
+]
+export const Navbar = async () => {
     return (
         <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-            <span>Home</span>
+            <Link href='/' className="flex items-center ">
+                <HomeIcon className="mr-2" />
+
+                <span>Home</span>
+            </Link>
             <div className="flex flex-1">
             </div>
-                <a className="mr-2" href="/about">About</a>
-                <a className="mr-2" href="/pricing">Pricing</a>
-                <a className="mr-2" href="/contact">Contact</a>
+            {
+                nevItems.map(({ path, text }) => (
+                    <Link key={path} href={path} className="mr-2">{text}</Link>
+                ))
+            }
+
         </nav>
     )
 }
